56d98a497e5b3e761510962cd1afa09a7bdf5538
everaldoinovus <everaldo@inovus.com.br>
2025-07-24 14:46:32 -0300

    Reverte para MySQL para compatibilidade com AWS RDS

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,6 @@
-// Arquivo: gerenciador-tarefas-api/src/config/database.js
-
+const mysql = require('mysql2/promise');
 require('dotenv').config();
-const { Pool } = require('pg');
 
-// Esta configuração é a ideal para se conectar a um PostgreSQL na nuvem
-// como o da Clever Cloud, pois já inclui o tratamento de SSL.
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
